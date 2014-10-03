51e43332d0a557a8eef2b6051cb4f6b194a39d0b
User to run commands <lherrada@centos-workstation.(none)>
2014-10-03 19:10:21 +0000

    Adding apache recipe

---
+++ cookbooks/apache/metadata.rb
@@ -0,0 +1,7 @@
+name             'apache'
+maintainer       'Melissa Corp.'
+maintainer_email 'mherrada@yahoo.com'
+license          'All rights reserved'
+description      'Installs/Configures apache'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
