631f5fa759eeb90182ab462934c90607c76da779
Luis Herrada <lherrada@Luiss-MacBook-Pro.local>
2014-10-08 09:17:31 -0700

    Updating metadata.rb

--- cookbooks/basic/metadata.rb
+++ cookbooks/basic/metadata.rb
@@ -1,6 +1,6 @@
 name             'basic'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Twitter'
+maintainer_email 'lherrada@yahoo.com'
 license          'All rights reserved'
 description      'Installs/Configures basic'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
