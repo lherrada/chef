f7e0e2b493fb588a34ac9df46d8d1a0d2b759c14
lherrada <lherrada@yahoo.com>
2014-10-08 12:05:31 -0700

    Update metadata.rb

--- cookbooks/basic/metadata.rb
+++ cookbooks/basic/metadata.rb
@@ -1,9 +1,7 @@
 name             'basic'
-<<<<<<< HEAD
 maintainer       'Twitter'
 =======
 maintainer       'Luis Herrada'
->>>>>>> aeda97c6697e0eb419f89dc60c82539cc775a533
 maintainer_email 'lherrada@yahoo.com'
 license          'All rights reserved'
 description      'Installs/Configures basic'
