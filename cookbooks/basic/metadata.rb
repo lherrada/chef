5078e4c636503518d45d2674f977d79b07b0acd7
lherrada <lherrada@yahoo.com>
2014-10-08 12:06:56 -0700

    Update metadata.rb

--- cookbooks/basic/metadata.rb
+++ cookbooks/basic/metadata.rb
@@ -1,9 +1,7 @@
 name             'basic'
 maintainer       'Twitter'
-=======
-maintainer       'Luis Herrada'
 maintainer_email 'lherrada@yahoo.com'
 license          'All rights reserved'
 description      'Installs/Configures basic'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0'
